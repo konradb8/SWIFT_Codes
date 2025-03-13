04f22f8ed2f9798ac7602156ffc9f7564fa71672
konradb8 <konradbugaj547@gmail.com>
2025-03-13 21:43:42 +0100

    A few integration tests added

--- src/test/java/io/github/konradb8/swift/swiftservice/controller/SwiftCodeControllerTest.java
+++ src/test/java/io/github/konradb8/swift/swiftservice/controller/SwiftCodeControllerTest.java
@@ -17,6 +17,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.io.IOException;
+import java.util.List;
 
 import static org.mockito.Mockito.doAnswer;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -98,4 +99,77 @@ public class SwiftCodeControllerTest {
                 .andExpect(status().is(500))
                 .andReturn();
     }
+
+    @Test
+    void testGetSwiftCodesByCountry_found() throws Exception {
+        SwiftCode swiftCode1 = new SwiftCode();
+        swiftCode1.setSwiftCode("ABCDUYXXXX");
+        swiftCode1.setCountryISO2("UY");
+        swiftCode1.setName("Bank UY");
+        swiftCode1.setIsHeadquarter(true);
+
+        SwiftCode swiftCode2 = new SwiftCode();
+        swiftCode2.setSwiftCode("XYZBUYXXXX");
+        swiftCode2.setCountryISO2("UY");
+        swiftCode2.setName("Bank XYZ");
+        swiftCode2.setIsHeadquarter(false);
+
+        swiftCodeRepository.saveAll(List.of(swiftCode1, swiftCode2));
+
+        mockMvc.perform(get("/v1/swift-codes/country/UY")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.countryISO2").value("UY"))
+                .andExpect(jsonPath("$.swiftCodes[0].swiftCode").value("ABCDUYXXXX"))
+                .andExpect(jsonPath("$.swiftCodes[1].swiftCode").value("XYZBUYXXXX"));
+    }
+
+    @Test
+    void testGetSwiftCodesByCountry_notFound() throws Exception {
+        mockMvc.perform(get("/v1/swift-codes/country/ZZ")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void testAddSwiftCode_ok() throws Exception {
+        String requestJson = """
+            {
+                "swiftCode": "QWIS92R3XXX",
+                "address": "1234 Toronto",
+                "bankName": "Bank of Canada",
+                "countryISO2": "CA",
+                "countryName": "Canada"
+            }
+            """;
+
+        mockMvc.perform(post("/v1/swift-codes")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestJson))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.message").value("SWIFT code added successfully"));
+    }
+
+    @Test
+    void testDeleteSwiftCode_ok() throws Exception {
+        SwiftCode swiftCode = new SwiftCode();
+        swiftCode.setSwiftCode("DELETE123XXX");
+        swiftCode.setCountryISO2("DE");
+        swiftCode.setName("Bank XXX");
+        swiftCode.setIsHeadquarter(false);
+        swiftCodeRepository.save(swiftCode);
+
+        mockMvc.perform(delete("/v1/swift-codes/DELETE123XXX"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.message").value("SWIFT code deleted successfully"));
+    }
+
+    @Test
+    void testDeleteSwiftCode_notFound() throws Exception {
+        mockMvc.perform(delete("/v1/swift-codes/INVALIDCODE"))
+                .andExpect(status().isNotFound());
+    }
+
+
+
 }
